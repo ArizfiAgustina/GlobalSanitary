ce87f4f1c3f13067e1ef41eda813da7762881325
ArizfiAgustina <arizfi.19069@mhs.unesa.ac.id>
2022-08-03 19:02:40 +0700

    globalsanitary

---
+++ HalamanBeranda/index.php
@@ -0,0 +1,173 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css"/>
+
+    <title>PT ABI</title>
+  </head>
+  <body style="background-color : #F9F2EA; ">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color : white;">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">Global Sanitary</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Beranda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="tentangkami.php">Tentang Kami</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="akunsaya.php">Akun saya</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- Akhir Navbar -->
+
+<!-- Jumbotron -->
+    <section class="jumbotron pt-5 pb-5 text-center" style="height : 500px; ">
+          <h1 class="display-4 fw-bold pt-5">Aromateraphy+ <br/>
+          Sebarkan aroma perlindungan</h1>
+          <div class="row">
+    <div class="col-md-6 offset-md-3 pt-4">
+            <p>Aromateraphy+ merupakan inovasi produk global sanitary terbaru yang tidak hanya memberikan aroma relaksasi yang menyegarkan dari berbagai varian aroma yang unik, tetapi  juga memberikan manfaat yang sama seperti disinfektan yaitu perlindungan dari penyebaran virus covid 19. </p>
+</div>
+          </div>
+    </section>
+
+<!-- Akhir Jumbotron -->
+
+        <div class="row align-items-center pb-5">
+            <div class="col">
+              <img src="img/1.png" width="203px">
+            </div>
+            <div class="col">
+            <img src="img/2.png" width="203px">
+            </div>
+            <div class="col">
+            <img src="img/3.png" width="203px">
+            </div>
+            <div class="col">
+            <img src="img/4.png" width="203px">
+            </div>
+            <div class="col">
+            <img src="img/5.png" width="203px">
+            </div>
+            <div class="col">
+            <img src="img/6.png" width="203px">
+            </div>
+        </div>
+
+        <div class="row justify-content-evenly pt-5 pb-5">
+           <div class="col-4">
+              <img src="img/paketa.jpeg" width="500px" title="Paket A" alt="Gambar Paket A">
+             </a>
+           </div>
+           <div class="col-4">
+              <img src="img/paketb.jpeg" width="500px" title="Paket B" alt="Gambar Paket B">
+             </a>
+           </div>
+        </div>
+
+        <img src="img/remote.png" width="100%" title="Detail Remote" alt="Gambar Detail Remote">
+
+        <div class="container">
+        <div class="tutorial pt-5 text-center">
+          <h1 style="color : #AE5D4A"; >Tutorial Pemakaian</h1>
+
+          <div class="container">
+
+          <div class="row align-items-top pt-5">
+            <div class="col-3 mx-auto">
+            <img src="img/tutorial/1.jpeg" width="200" alt="Gambar Tutorial Step 1" >
+            <p class="pt-3">Sambungkan kabel cas dengan air humidifier diffuser</p>
+            </div>
+            <div class="col-3 mx-auto">
+            <img src="img/tutorial/2.jpeg" width="200" alt="Gambar Tutorial Step 2">
+            <p class="pt-3">Tuangkan cairan aromateraphy+ ke dalam tabung difusser. Isi cairan sebanyak yang diinginkan</p>
+            </div>
+            <div class="col-3 mx-auto">
+            <img src="img/tutorial/3.jpeg" width="200" alt="Gambar Tutorial Step 3">
+            <p class="pt-3">Pastikan tabung diffuser tertutup dengan benar</p>
+            </div>
+          </div>
+
+          <div class="row align-items-top pt-5 ">
+            <div class="col-3 mx-auto">
+            <img src="img/tutorial/4.jpeg" width="200" alt="Gambar Tutorial Step 4" >
+            <p class="pt-3">Sambungkan kabel cas dengan stop kontak</p>
+            </div>
+            <div class="col-3 mx-auto">
+            <img src="img/tutorial/5.jpeg" width="200" alt="Gambar Tutorial Step 5">
+            <p class="pt-3">Nyalakan diffuser menggunakan remote atau tombol di bagian belakang diffuser</p>
+            </div>
+            <div class="col-3 mx-auto">
+            <img src="img/tutorial/6.jpeg" width="200" alt="Gambar Tutorial Step 6">
+            <p class="pt-3">Jika perlu, nyalakan lampu pada diffuser, tekan tombol “light” pada remote</p>
+            </div>
+          </div>
+
+          <div class="row align-items-top pt-5 pb-5">
+            <div class="col-3 mx-auto">
+            <img src="img/tutorial/7.jpeg" width="200" alt="Gambar Tutorial Step 7" >
+            <p class="pt-3">Atur lama diffuser sesuai keinginan, tekan tombol “timer” pada remote</p>
+            </div>
+            <div class="col-3 mx-auto">
+            <img src="img/tutorial/8.jpeg" width="200" alt="Gambar Tutorial Step 8">
+            <p class="pt-3">Diffuser siap digunakan</p>
+            </div>
+            <div class="col-3 mx-auto">
+            <img src="img/tutorial/9.jpeg" width="200" alt="Gambar Tutorial Step 9">
+            <p class="pt-3">Tekan tombol “On/Off” pada remote untuk mematikan diffuser</p>
+            </div>
+          </div>
+</div>
+</div>
+</div>
+
+          <img src="img/petunjuk.png" width="100%" title="Petunjuk perawatan" alt="Gambar Petunjuk Perawatan">
+
+          <section class="footer" style="background-color : #F8F799; font-colo">
+            <div class="container" >
+            <div class="row pt-5 pb-5">
+              <div class="col-4">
+                <p>Social Media</p>
+              </div>
+              <div class="col-8">
+                <p>Telp 08123456789</p>
+                <br/>
+                <p>DUSUN LEKERREJO RT.03/RW.03, DS. DADAP KUNING, KEC. CERME, KAB. GRESIK 61171</p>
+                <br/>
+                <p>PT. ADYUTA BUMI INDONESIA adalah badan usaha berpengalaman yang mengerjakan proyek nasional. PT. ADYUTA BUMI INDONESIA dapat mengerjakan proyek-proyek dengan sub klasifikasi, seperti jasa pelaksana untuk kontruksi bangunan komersial, jasa pelaksana untuk kontruksi jalan raya, jasa pelaksana konstruksi pekerjaan jembatan, jalan Layang, terowongan dan subways.</p>
+              </div>
+            </div>
+            </div>
+</section>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
