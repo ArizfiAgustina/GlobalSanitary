54e2b85a145168fbc6e67e4398219f4998a0821f
ArizfiAgustina <arizfi.19069@mhs.unesa.ac.id>
2022-08-10 15:52:57 +0700

    designpage

---
+++ designpage/landingpage.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="grid.css">
+    <link rel="stylesheet" href="form.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css"/>
+
+    <title>PT.ABI</title>
+  </head>
+  <body style="background-color : #F9F2EA; ">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color : white;">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">Global Sanitary</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Beranda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="tentangkami.php">Tentang Kami</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="akunsaya.php">Akun saya</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- Akhir Navbar -->
+
+<div class="hero-image">
+  <div class="hero-text ">
+    <h1>Aromateraphy+ 
+    <br>Sebarkan aroma perlindungan<br/></h1>
+    <p>Aromateraphy+ merupakan inovasi produk global sanitary terbaru yang tidak hanya memberikan aroma relaksasi yang menyegarkan dari berbagai varian aroma yang unik, tetapi  juga memberikan manfaat yang sama seperti disinfektan yaitu perlindungan dari penyebaran virus dan bakteri.</p>
+  </div>
+</div>
+
+<h2>Layanan Kami</h2>
+
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="img/delivery.png">
+      <h3>Pengiriman Cepat</h3>
+      <p>Kami bekerja sama dengan  jasa pengiriman barang yang terpercaya, sehingga pesanan diterima teapt waktu</p>
+
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="img/box.png">
+      <h3>Kemasan Aman</h3>
+      <p>Kami menggunakan kemasan yang sangat aman sehingga barang tidak mudah rusak</p>
+    </div>
+  </div>
+  
+  <div class="column">
+    <div class="card">
+      <img src="img/akses.png">
+      <h3>Akses</h3>
+      <p>Pembeli dapat memesan melalui Website ini, daftarkan diri Anda menjadi member dan dapatkan harga khusus dari kami </p>
+    </div>
+  </div>
+</div>
+
+
+<div class="center-image" style="background-image: url('img/bst.png');">
+  <div class="main">
+  <div class="img-box">
+    <img class="modified-img" src="img/5.png">
+    <p style="text-align: center; color: white;">VARIAN SERAI</p>
+  </div>
+
+  <div class="img-box">
+    <img class="modified-img" src="img/6.png">
+    <p style="text-align: center; color: white;">VARIAN LAVENDER</p>
+  </div>
+
+  </div>
+</div>
+
+<div class="login">
+  <h2>MEMBER</h2>
+  <form>
+  <div class="input-group">
+    <input type="text" name="" placeholder="Masukkan Username">
+  </div>
+  <div class="input-group">
+    <input type="email" name="" placeholder="Masukkan Email">
+  </div>
+  <div class="input-group">
+    <input type="password" name="" placeholder="Masukkan Password">
+  </div>
+  <div class="input-group">
+    <input type="submit" value="Daftar">
+  <p>Dapatkan Harga Khusus dengan Menjadi Member Kami</p>
+  </form>
+  </div>
+  
+</div>
+
+<div class="footer-content">
+  <div class="content-social">
+    <p>Instagram <br> Tiktok <br> Facebook <br> E-mail</p>
+  </div>
+
+  <div class="content-contact">
+    <p>Telp 08123456789 <br> DUSUN LEKERREJO RT.03/RW.03, DS. DADAP KUNING, KEC. CERME, KAB. GRESIK 61171 <br> PT. ADYUTA BUMI INDONESIA adalah badan usaha berpengalaman yang mengerjakan proyek nasional. PT. ADYUTA BUMI INDONESIA dapat mengerjakan proyek-proyek dengan sub klasifikasi, seperti jasa pelaksana untuk kontruksi bangunan komersial, jasa pelaksana untuk kontruksi jalan raya, jasa pelaksana konstruksi pekerjaan jembatan, jalan Layang, terowongan dan subways.</p>
+  </div>
+  
+  <button class="button">Login Admin</button>
+</div>
+ 
+    
+</body>
+</html>
