844233b63f72c539cf93930e798adc8622b91347
ArizfiAgustina <arizfi.19069@mhs.unesa.ac.id>
2022-08-12 15:25:09 +0700

    menambahkan file baru

---
+++ HalamanBeranda/katalogB.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="cat1.css">
+	<title>Paket B</title>
+</head>
+<body>
+	<div class="header">
+  		<a href="#default" class="logo">Global Sanitary</a>
+  	<div class="header-right">
+    	<a class="active" href="#home">Beranda</a>
+      <a href="#about">Tentang Kami</a>
+    	<a href="#contact">Akun</a>
+    	
+  </div>
+
+ <div class="center-image">
+  <div class="main">
+  <div class="img-box">
+    <img class="modified-img" src="img/paketb.png">
+  </div>
+
+ <div class="text-content">
+  <div class="content-description">
+    <h2>Paket B</h2>
+    <h3>Deskripsi :</h3>
+    <p>
+      * Kapasitas 500 ml <br>
+      * Otomatis mati sendiri jika cairan habis <br>
+      * Dapat disetting timer: <br>
+    </p>
+  </div>
+
+  <div class="content-bottom">
+    <p>
+        1. Alat dapat disetting 60 menit nyala 60 menit mati 
+     kemudian nyala kembali <br>
+        2. Alat dapat disetting 120 menit nyala 120 menit mati 
+      kemudian  nyala kembali <br>
+        3. Alat dapat disetting 180 menit nyala 180 menit mati
+      kemudian nyala kembali <br>
+    </p>
+     <p>
+      <strong>Member:</strong> <br>
+      500ml     <i>Rp. 150.000,-</i> <br>
+      1 Liter   <i>Rp. 130.000,-</i> <br>
+    </p>
+    <p>
+      <strong>Non Member:</strong> <br>
+      500ml     <i>Rp. 200.000,-</i> <br>
+      1 Liter   <i>Rp. 200.000,-</i> <br>
+    </p>
+    <button class="button">Beli</button>
+  </div>
+
+   
+ </div>
+</div>
+</div>
+
+</div>
+</body>
+</html>
