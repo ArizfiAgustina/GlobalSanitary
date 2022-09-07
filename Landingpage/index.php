4b3a9821bbd327e249e091fe44e8bf00a9c8c479
elfalbirri <elfaalbirri19@gmail.com>
2022-09-07 14:04:17 +0700

    update banyak banget

---
+++ Landingpage/index.php
@@ -0,0 +1,156 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css"/>
+
+    <title>Global Sanitary</title>
+  </head>
+  <body style="background-color : #F9F2EA; ">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color : white;">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">Global Sanitary</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Beranda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="tentangkami.php">Tentang Kami</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../Dashboard/login.php">Login Admin</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- Akhir Navbar -->
+
+<!-- Jumbotron -->
+    <section class="jumbotron pt-5 pb-5 text-center" style="height : 500px; ">
+          <h1 class="display-4 fw-bold pt-5">Aromateraphy+ <br/>
+          Sebarkan aroma perlindungan</h1>
+          <div class="row">
+    <div class="col-md-6 offset-md-3 pt-4">
+            <p>Aromateraphy+ merupakan inovasi produk global sanitary terbaru yang tidak hanya memberikan aroma relaksasi yang menyegarkan dari berbagai varian aroma yang unik, tetapi  juga memberikan manfaat yang sama seperti disinfektan yaitu perlindungan dari penyebaran virus dan kuman. </p>
+</div>
+          </div>
+    </section>
+
+<!-- Akhir Jumbotron -->
+
+  <section class="layanan" style="background-color : #F9F2EA;">
+  <div class="container">
+    <h3 class="text-center pt-5" style="color : #9F2E32;">Layanan Kami</h3>
+      <div class="row align-items-center pt-5 pb-5">
+          <div class="col text-center">
+              <div class="card align-items-center" style="width: 18rem; background-color: #F8F799;">
+                <img src="img/delivery.png" class="card-img-top" alt="..." style="width: 150px;">
+                <div class="card-body">
+                  <h5 style="color : #9F2E32;">PENGIRIMAN CEPAT</h5>
+                  <p class="card-text">Kami bekerja sama dengan  jasa pengiriman barang yang terpercaya, sehingga pesanan diterima teapt waktu</p>
+                </div>
+              </div>
+          </div>
+          <div class="col text-center">
+              <div class="card align-items-center" style="width: 18rem; background-color: #F8F799;">
+                <img src="img/box.png" class="card-img-top" alt="..." style="width: 150px;">
+                <div class="card-body">
+                  <h5 style="color : #9F2E32;">KEMASAN AMAN</h5>
+                  <p class="card-text">Kami menggunakan kemasan yang sangat aman sehingga barang tidak mudah rusak.</p>
+                </div>
+              </div>
+          </div>
+          <div class="col text-center">
+            <div class="card align-items-center" style="width: 18rem; background-color: #F8F799;">
+                <img src="img/akses.png" class="card-img-top" alt="..." style="width: 120px;">
+                <div class="card-body">
+                  <h5 style="color : #9F2E32;">AKSES</h5>
+                  <p class="card-text">Pembeli dapat memesan melalui Website ini, daftarkan diri Anda menjadi member dan dapatkan harga khusus dari kami </p>
+                </div>
+              </div>
+          </div>
+        </div>
+  </div>
+  </section>
+
+  <section class="bestseller">
+  <div class="center-image" style="background-color: #9F2E32; height:570px;">
+    <div class="container text-center">
+      <div class="best pt-5 pb-5 text-center">
+        <h1 style="color: white;">BEST SELLER</h1>
+        <div class="row justify-content-evenly pt-5">
+          <div class="col-4">
+            <img src="img/5.png" width="300">
+              <h4 style="color: white;">Varian Serai</h4>
+          </div>
+          <div class="col-4">
+            <img src="img/6.png" width="300">
+              <h4 style="color: white;">Varian Lavender</h4>
+          </div>
+        </div>
+      </div>
+  </div>
+</section>
+
+<section class="input">
+  <div class="container text-center pt-5">
+      <h3>DAFTAR MEMBER</h3>
+  </div>
+
+  <div class="container pt-3 pb-5">
+      <form>
+        <div class="mb-3">
+          <label for="exampleUsername1" class="form-label">Nama Pengguna</label>
+          <input type="text" class="form-control" id="exampleUsername1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <a class="btn btn-primary" href="../Homepage/index.php" role="button">Submit</a>
+    </form>
+  </div>
+  </div>
+</section>
+
+<section class="footer" style="background-color : #F8F799;">
+            <div class="container" >
+            <div class="row pt-5 pb-5">
+              <div class="col-4">
+                <p>Social Media</p>
+              </div>
+              <div class="col-8">
+                <p>Telp 08123456789</p>
+                <br/>
+                <p>DUSUN LEKERREJO RT.03/RW.03, DS. DADAP KUNING, KEC. CERME, KAB. GRESIK 61171</p>
+                <br/>
+                <p>PT. ADYUTA BUMI INDONESIA adalah badan usaha berpengalaman yang mengerjakan proyek nasional. PT. ADYUTA BUMI INDONESIA dapat mengerjakan proyek-proyek dengan sub klasifikasi, seperti jasa pelaksana untuk kontruksi bangunan komersial, jasa pelaksana untuk kontruksi jalan raya, jasa pelaksana konstruksi pekerjaan jembatan, jalan Layang, terowongan dan subways.</p>
+              </div>
+              </div>
+            </div>
+</section>
+
+
+</body>
+</html>
